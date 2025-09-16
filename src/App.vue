f4bb7b93aca054149374f2ab3b316b71b78c9134
Coen Hallie <me@coenhallie.com>
2025-09-15 22:02:25 -0400

    upp version

--- src/App.vue
+++ src/App.vue
@@ -1735,7 +1735,7 @@ watch(
           <span
             class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800 border border-orange-200"
           >
-            BETA v2.0
+            BETA v2.1
           </span>
         </div>
 
